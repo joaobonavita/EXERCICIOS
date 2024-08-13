07f27d0366a365e2fe38c7086f3040a6dcb0bf3a
24014338 <joao.vbmg@puccampinas.edu.br>
2024-08-13 20:54:59 -0300

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -1,9 +1,9 @@
-#include <studio.h>
+#include <iostream>
 int main() {
 int visitantes;
 //Solicita o número de visitantes ao recepcionista
 printf("Digite o número de visitantes de hoje");
-scanf("%d", &visitantes)
+scanf("%d", &visitantes);
 //Exibe uma mensagem de confirmação
 printf("Número de visitantes registrado: %d\n", visitantes);
 return 0;
